cc411c20cdaea44f23e56d63d1a6ee9e04de5e4a
Sasa89 <sasa.brezovac@cobeisfresh.com>
2021-10-27 14:48:02 +0200

    Initial Commit

---
+++ CustomLoginDemoFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomLoginDemoFirebase
+//
+//  Created by Saša Brezovac on 27.10.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
